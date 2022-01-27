d203e1163d27b10ceba2443aa9128a6ee3ee78a1
Ange Yaghi <me@angeyaghi.com>
2022-01-26 21:59:37 -0500

    Added .gitignore, fixed cmake build with ffmpeg

--- cmake/FindFFmpeg.cmake
+++ cmake/FindFFmpeg.cmake
@@ -8,31 +8,22 @@ else()
   set(VC_LIB_PATH_SUFFIX win32)
 endif()
 
-#find_path(AVCODEC_INCLUDE_DIR test.txt
-#    HINTS
-#    PATH_SUFFIXES
-#        "${VC_LIB_PATH_SUFFIX}"
-#        include
-#        libavcodec)
-#    message("hello: '$ENV{path}'")
+find_path(AVCODEC_INCLUDE_DIR libavcodec/avcodec.h 
+    PATH_SUFFIXES
+        "${VC_LIB_PATH_SUFFIX}/include")
 find_library(AVCODEC_LIBRARY avcodec)
 
-if(NOT SDL2_DIR)
-  set(SDL2_DIR "" CACHE PATH "SDL2 directory")
-endif()
-find_path(AVCODEC_INCLUDE_DIR SDL.h
-  HINTS
-    ENV SDL2DIR
-    ${SDL2_DIR}
-  PATH_SUFFIXES SDL2
-                include/SDL2 include)
-message("e: ${AVCODEC_INCLUDE_DIR}")
-
-find_path(AVFORMAT_INCLUDE_DIR libavformat/avformat.h)
+find_path(AVFORMAT_INCLUDE_DIR libavformat/avformat.h
+    PATH_SUFFIXES
+        "${VC_LIB_PATH_SUFFIX}/include")
 find_library(AVFORMAT_LIBRARY avformat)
 
-find_path(AVUTIL_INCLUDE_DIR libavutil/avutil.h)
+find_path(AVUTIL_INCLUDE_DIR libavutil/avutil.h
+    PATH_SUFFIXES
+        "${VC_LIB_PATH_SUFFIX}/include")
 find_library(AVUTIL_LIBRARY avutil)
 
-find_path(AVDEVICE_INCLUDE_DIR libavdevice/avdevice.h)
+find_path(AVDEVICE_INCLUDE_DIR libavdevice/avdevice.h
+    PATH_SUFFIXES
+        "${VC_LIB_PATH_SUFFIX}/include")
 find_library(AVDEVICE_LIBRARY avdevice)
--- include/ffmpeg.h
+++ include/ffmpeg.h
@@ -2,14 +2,14 @@
 #define ATG_DIRECT_TO_VIDEO_FFMPEG_H
 
 extern "C" {
-    #include <avassert.h>
-    #include <channel_layout.h>
-    #include <opt.h>
-    #include <mathematics.h>
-    #include <timestamp.h>
-    #include <avcodec.h>
-    #include <swscale.h>
-    #include <swresample.h>
+    #include <libavutil/avassert.h>
+    #include <libavutil/channel_layout.h>
+    #include <libavutil/opt.h>
+    #include <libavutil/mathematics.h>
+    #include <libavutil/timestamp.h>
+    #include <libavcodec/avcodec.h>
+    #include <libswscale/swscale.h>
+    #include <libswresample/swresample.h>
 }
 
 #endif /* ATG_DIRECT_TO_VIDEO_FFMPEG_H */
